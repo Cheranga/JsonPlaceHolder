3c1ddf6a2e6f8708c20b1539a19feccebd59744a
Cheranga Hatangala <cheranga@gmail.com>
2019-03-02 06:40:51 +1100

    Add project files.

---
+++ JsonPlaceHolder/DTO/Responses/GetAllPostsResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JsonPlaceHolder.DTO.Assets;
+
+namespace JsonPlaceHolder.DTO.Responses
+{
+    public class GetAllPostsResponse
+    {
+        public GetAllPostsResponse()
+        {
+            Posts = new List<Post>();
+        }
+
+        public List<Post> Posts { get; set; }
+
+        public bool IsValid()
+        {
+            return Posts != null && Posts.Any();
+        }
+    }
+}
---
+++ JsonPlaceHolder/Services/IPostsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JsonPlaceHolder.DTO.Responses;
+
+namespace JsonPlaceHolder.Services
+{
+    public interface IPostsService
+    {
+        Task<GetAllPostsResponse> GetAllPostsAsync();
+    }
+}
